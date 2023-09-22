43c912bb4a27671e27bd4d3786d0460643582f36
tnixc <lauenochlau@gmail.com>
2023-09-22 10:54:53 -0400

    e

---
+++ src/components/nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>hi</h1>
+</template>
+
+<script>
+export default {
+  name: "navBar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
