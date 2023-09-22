b78829cd3915900fa1db056756f3831fa8e43755
tnixc <lauenochlau@gmail.com>
2023-09-22 11:07:53 -0400

    colors

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,14 +1,15 @@
 <template>
-  <h1>hi</h1>
+  <div id="navContainer">
+  </div>
 </template>
 
 <script>
+
 export default {
   name: "navBar",
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
