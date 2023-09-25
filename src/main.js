d2fde0a3038f96507b42196b5975b65e48f611bf
tnixc <lauenochlau@gmail.com>
2023-09-25 13:25:21 -0400

    redirect / to work

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import * as VueRouter from 'vue-router';
 
 const routes = [
+  { path: '/', component: () => import('./components/work-history.vue') },
   { path: '/work', component: () => import('./components/work-history.vue') },
   { path: '/about', component: () => import('./components/about-me.vue') },
   { path: '/contact', component: () => import('./components/contact-me.vue') },
