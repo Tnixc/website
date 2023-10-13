74266824b5ad3094ead653ff82db59aa0da2ac03
Tnixc <tnixxc@gmail.com>
2023-10-13 19:27:00 -0400

    hell

--- src/components/about-me.vue
+++ src/components/about-me.vue
@@ -65,24 +65,40 @@
         </h4>
         <h2 id="software-head">> Software I use:</h2>
         <div id="softwares">
-          <img src="https://code.visualstudio.com/assets/images/code-stable.png" alt="">
-          <img src="https://i.redd.it/ne6ukkej06t71.png" alt="">
-          <img src="https://cdn-icons-png.flaticon.com/512/2/2235.png" alt="">
-          <img src="https://cdn.jim-nielsen.com/macos/512/affinity-photo-2-2023-02-14.png" alt="">
-          <img src="https://cdn.jim-nielsen.com/macos/512/affinity-designer-2-2023-02-14.png" alt="">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Procreate-icon.png" alt="">
-          <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/9c/6d/31/9c6d310b-c0f0-3184-e53f-e09798bc47b4/AppIcon-0-0-1x_U007epad-0-0-0-0-0-0-85-220.png/512x512bb.jpg" alt="">
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>Visual Studio Code</title>
+            <path
+              d="M23.15 2.587L18.21.21a1.494 1.494 0 0 0-1.705.29l-9.46 8.63-4.12-3.128a.999.999 0 0 0-1.276.057L.327 7.261A1 1 0 0 0 .326 8.74L3.899 12 .326 15.26a1 1 0 0 0 .001 1.479L1.65 17.94a.999.999 0 0 0 1.276.057l4.12-3.128 9.46 8.63a1.492 1.492 0 0 0 1.704.29l4.942-2.377A1.5 1.5 0 0 0 24 20.06V3.939a1.5 1.5 0 0 0-.85-1.352zm-5.146 14.861L10.826 12l7.178-5.448v10.896z" />
+          </svg>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>Windows 11</title>
+            <path d="M0,0H11.377V11.372H0ZM12.623,0H24V11.372H12.623ZM0,12.623H11.377V24H0Zm12.623,0H24V24H12.623" />
+          </svg>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>Apple</title>
+            <path
+              d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701" />
+          </svg>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>Affinity Photo</title>
+            <path
+              d="M10.44 0l-.48.831 5.88 10.185L22.2 0zm12.84 0l-8.577 14.856H24V.711A.72.72 0 0023.28 0zM9.42 1.767L5.76 8.106h7.32zm1.563 7.257h-.018c-.36.005-.7.216-.879.523l-1.083 1.88-.008.014a1.052 1.052 0 000 1.02 16710.388 16710.388 0 001.093 1.894c.184.31.53.5.885.501.002 0 1.38.002 2.067-.001.36-.005.699-.205.878-.512.364-.631.731-1.261 1.093-1.894.176-.314.17-.703-.007-1.011l-.01-.015-1.078-1.87-.006-.009a1.053 1.053 0 00-.879-.52h-.012zM5.22 9.04L0 18.082v.39l.003 4.871a.72.72 0 00.662.655L9.3 9.04zm2.94 3.845L1.736 24h12.84zm2.757 2.906L15.657 24h7.623a.72.72 0 00.72-.72v-7.488Z" />
+          </svg>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>Affinity Designer</title>
+            <path
+              d="M10.44 0L0 18.083v5.197a.72.72 0 00.713.72h10.023L5.7 15.277 14.52 0zm5.16 0l-4.86 8.418 3.718 6.439H24V.718A.72.72 0 0023.28 0zm-5.4 9.353l-2.064 3.575a1.289 1.289 0 000 1.288c.23.4.656.64 1.117.64h4.125zm-3.122 6.44L11.816 24h11.471a.72.72 0 00.713-.718v-7.49Z" />
+          </svg>
         </div>
         <h2 id="games-head">> Games I play:</h2>
         <div id="games">
-          <img src="https://preview.redd.it/you-have-an-ugly-gray-creeper-instead-of-a-minecraft-icon-v0-y83ppc5i6r4b1.png?width=1024&format=png&auto=webp&s=12576cea991cd7c24bd277c1c43800e81ea0e73a" alt="">
-          <img src="https://docs.modrinth.com/img/logo.svg" alt="">
-          <img src="https://i.imgur.com/GgmnLVw.png" alt="">
-          <img src="https://b.thumbs.redditmedia.com/trqWzHVpzC1gYZzxdunc1IcPfGPjHF9it-JwXXKtaMo.png" alt="">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Cyberpunk_2077_logo.svg/2560px-Cyberpunk_2077_logo.svg.png" alt="">
-          <img src="https://www.rpgfan.com/wp-content/uploads/2020/10/VA-11-HALL-A-Cyberpunk-Bartender-Action-Logo-001.jpg" alt="">
-          <img src="https://static.wikia.nocookie.net/segamusic/images/4/43/Project_SEKAI_COLORFUL_STAGE%21_feat._Hatsune_Miku_Logo.png" alt="">
-      </div>
+          <img :src="require('@/assets/cyberpunk.png')" alt="cyberpunk" />
+          <img :src="require('@/assets/modrinth.png')" alt="modrinth" />
+          <img :src="require('@/assets/Genshin_Impact.webp')" alt="genshin impact" />
+          <img :src="require('@/assets/va11halla.jpg')" alt="va11halla" />
+          <img :src="require('@/assets/pjsk.webp')" alt="project sekai" />
+          <img :src="require('@/assets/osu.png')" alt="osu" />
+        </div>
       </div>
       <h1 class="subtitle">> Stuff I know:</h1>
       <div id="cards">
@@ -90,7 +106,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-solid fa-code-merge"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>Git</title>
+                  <path
+                    d="M23.546 10.93L13.067.452c-.604-.603-1.582-.603-2.188 0L8.708 2.627l2.76 2.76c.645-.215 1.379-.07 1.889.441.516.515.658 1.258.438 1.9l2.658 2.66c.645-.223 1.387-.078 1.9.435.721.72.721 1.884 0 2.604-.719.719-1.881.719-2.6 0-.539-.541-.674-1.337-.404-1.996L12.86 8.955v6.525c.176.086.342.203.488.348.713.721.713 1.883 0 2.6-.719.721-1.889.721-2.609 0-.719-.719-.719-1.879 0-2.598.182-.18.387-.316.605-.406V8.835c-.217-.091-.424-.222-.6-.401-.545-.545-.676-1.342-.396-2.009L7.636 3.7.45 10.881c-.6.605-.6 1.584 0 2.189l10.48 10.477c.604.604 1.582.604 2.186 0l10.43-10.43c.605-.603.605-1.582 0-2.187" />
+                </svg>
                 <div class="card-info-title">
                   <h3>Git</h3>
                   <h4>and github too!</h4>
@@ -103,7 +123,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-brands fa-css3-alt"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>CSS3</title>
+                  <path
+                    d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z" />
+                </svg>
                 <div class="card-info-title">
                   <h3>CSS</h3>
                   <h4>This is not hell</h4>
@@ -116,7 +140,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-brands fa-html5"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>HTML5</title>
+                  <path
+                    d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z" />
+                </svg>
                 <div class="card-info-title">
                   <h3>HTML</h3>
                   <h4>The vanilla way</h4>
@@ -129,7 +157,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-brands fa-square-js"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>JavaScript</title>
+                  <path
+                    d="M0 0h24v24H0V0zm22.034 18.276c-.175-1.095-.888-2.015-3.003-2.873-.736-.345-1.554-.585-1.797-1.14-.091-.33-.105-.51-.046-.705.15-.646.915-.84 1.515-.66.39.12.75.42.976.9 1.034-.676 1.034-.676 1.755-1.125-.27-.42-.404-.601-.586-.78-.63-.705-1.469-1.065-2.834-1.034l-.705.089c-.676.165-1.32.525-1.71 1.005-1.14 1.291-.811 3.541.569 4.471 1.365 1.02 3.361 1.244 3.616 2.205.24 1.17-.87 1.545-1.966 1.41-.811-.18-1.26-.586-1.755-1.336l-1.83 1.051c.21.48.45.689.81 1.109 1.74 1.756 6.09 1.666 6.871-1.004.029-.09.24-.705.074-1.65l.046.067zm-8.983-7.245h-2.248c0 1.938-.009 3.864-.009 5.805 0 1.232.063 2.363-.138 2.711-.33.689-1.18.601-1.566.48-.396-.196-.597-.466-.83-.855-.063-.105-.11-.196-.127-.196l-1.825 1.125c.305.63.75 1.172 1.324 1.517.855.51 2.004.675 3.207.405.783-.226 1.458-.691 1.811-1.411.51-.93.402-2.07.397-3.346.012-2.054 0-4.109 0-6.179l.004-.056z" />
+                </svg>
                 <div class="card-info-title">
                   <h3>JavaScript</h3>
                   <h4>Not good at javascript at all</h4>
@@ -142,7 +174,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-brands fa-vuejs"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>Vue.js</title>
+                  <path
+                    d="M24,1.61H14.06L12,5.16,9.94,1.61H0L12,22.39ZM12,14.08,5.16,2.23H9.59L12,6.41l2.41-4.18h4.43Z" />
+                </svg>
                 <div class="card-info-title">
                   <h3>Vue</h3>
                   <h4>Used it for this website!</h4>
@@ -155,7 +191,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-brands fa-python"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>Python</title>
+                  <path
+                    d="M14.25.18l.9.2.73.26.59.3.45.32.34.34.25.34.16.33.1.3.04.26.02.2-.01.13V8.5l-.05.63-.13.55-.21.46-.26.38-.3.31-.33.25-.35.19-.35.14-.33.1-.3.07-.26.04-.21.02H8.77l-.69.05-.59.14-.5.22-.41.27-.33.32-.27.35-.2.36-.15.37-.1.35-.07.32-.04.27-.02.21v3.06H3.17l-.21-.03-.28-.07-.32-.12-.35-.18-.36-.26-.36-.36-.35-.46-.32-.59-.28-.73-.21-.88-.14-1.05-.05-1.23.06-1.22.16-1.04.24-.87.32-.71.36-.57.4-.44.42-.33.42-.24.4-.16.36-.1.32-.05.24-.01h.16l.06.01h8.16v-.83H6.18l-.01-2.75-.02-.37.05-.34.11-.31.17-.28.25-.26.31-.23.38-.2.44-.18.51-.15.58-.12.64-.1.71-.06.77-.04.84-.02 1.27.05zm-6.3 1.98l-.23.33-.08.41.08.41.23.34.33.22.41.09.41-.09.33-.22.23-.34.08-.41-.08-.41-.23-.33-.33-.22-.41-.09-.41.09zm13.09 3.95l.28.06.32.12.35.18.36.27.36.35.35.47.32.59.28.73.21.88.14 1.04.05 1.23-.06 1.23-.16 1.04-.24.86-.32.71-.36.57-.4.45-.42.33-.42.24-.4.16-.36.09-.32.05-.24.02-.16-.01h-8.22v.82h5.84l.01 2.76.02.36-.05.34-.11.31-.17.29-.25.25-.31.24-.38.2-.44.17-.51.15-.58.13-.64.09-.71.07-.77.04-.84.01-1.27-.04-1.07-.14-.9-.2-.73-.25-.59-.3-.45-.33-.34-.34-.25-.34-.16-.33-.1-.3-.04-.25-.02-.2.01-.13v-5.34l.05-.64.13-.54.21-.46.26-.38.3-.32.33-.24.35-.2.35-.14.33-.1.3-.06.26-.04.21-.02.13-.01h5.84l.69-.05.59-.14.5-.21.41-.28.33-.32.27-.35.2-.36.15-.36.1-.35.07-.32.04-.28.02-.21V6.07h2.09l.14.01zm-6.47 14.25l-.23.33-.08.41.08.41.23.33.33.23.41.08.41-.08.33-.23.23-.33.08-.41-.08-.41-.23-.33-.33-.23-.41-.08-.41.08z" />
+                </svg>
                 <div class="card-info-title">
                   <h3>Python</h3>
                   <h4>I think I actually know this</h4>
@@ -168,7 +208,11 @@
           <div class="card-content">
             <div class="card-info-wrapper">
               <div class="card-info">
-                <i class="fa-brands fa-figma"></i>
+                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <title>Figma</title>
+                  <path
+                    d="M15.852 8.981h-4.588V0h4.588c2.476 0 4.49 2.014 4.49 4.49s-2.014 4.491-4.49 4.491zM12.735 7.51h3.117c1.665 0 3.019-1.355 3.019-3.019s-1.355-3.019-3.019-3.019h-3.117V7.51zm0 1.471H8.148c-2.476 0-4.49-2.014-4.49-4.49S5.672 0 8.148 0h4.588v8.981zm-4.587-7.51c-1.665 0-3.019 1.355-3.019 3.019s1.354 3.02 3.019 3.02h3.117V1.471H8.148zm4.587 15.019H8.148c-2.476 0-4.49-2.014-4.49-4.49s2.014-4.49 4.49-4.49h4.588v8.98zM8.148 8.981c-1.665 0-3.019 1.355-3.019 3.019s1.355 3.019 3.019 3.019h3.117V8.981H8.148zM8.172 24c-2.489 0-4.515-2.014-4.515-4.49s2.014-4.49 4.49-4.49h4.588v4.441c0 2.503-2.047 4.539-4.563 4.539zm-.024-7.51a3.023 3.023 0 0 0-3.019 3.019c0 1.665 1.365 3.019 3.044 3.019 1.705 0 3.093-1.376 3.093-3.068v-2.97H8.148zm7.704 0h-.098c-2.476 0-4.49-2.014-4.49-4.49s2.014-4.49 4.49-4.49h.098c2.476 0 4.49 2.014 4.49 4.49s-2.014 4.49-4.49 4.49zm-.097-7.509c-1.665 0-3.019 1.355-3.019 3.019s1.355 3.019 3.019 3.019h.098c1.665 0 3.019-1.355 3.019-3.019s-1.355-3.019-3.019-3.019h-.098z" />
+                </svg>
                 <div class="card-info-title">
                   <h3>Figma</h3>
                   <h4>Good stuff</h4>
@@ -181,11 +225,9 @@
       <h1 class="subtitle contact">> Links/Contact</h1>
       <div class="left">
         <ol>
-          <li>  
-            <i class="fa-brands fa-github"></i
-            ><a href="https://github.com/Tnixc"
-              >Tnixc<i class="fa-solid fa-up-right-from-square"></i
-            ></a>
+          <li>
+            <i class="fa-brands fa-github"></i><a href="https://github.com/Tnixc">Tnixc<i
+                class="fa-solid fa-up-right-from-square"></i></a>
           </li>
           <li>
             <i class="fa-brands fa-discord"></i>
@@ -193,9 +235,7 @@
           </li>
           <li>
             <i class="fa-solid fa-envelope"></i>
-            <a id="copy-email"
-              >tnixxc@gmail.com<i class="fa-solid fa-copy"></i
-            ></a>
+            <a id="copy-email">tnixxc@gmail.com<i class="fa-solid fa-copy"></i></a>
           </li>
         </ol>
       </div>
@@ -267,9 +307,10 @@ body {
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-    overflow: hidden;
+  overflow: hidden;
 }
-#softwares{
+
+#softwares {
   display: flex;
   flex-wrap: wrap;
   align-items: center;
@@ -277,29 +318,22 @@ body {
   position: relative;
   z-index: 9;
 }
-#software-head{
+
+#software-head {
   padding-top: 20px;
 }
-#softwares > img{
+
+#softwares>svg {
   height: 36px;
-  border-radius: 8px;
-}
-#softwares > img:nth-child(1),#softwares > img:nth-child(2){
-  padding: 4px;
-  transform: translateY(2px);
+  fill: $accent;
+  border-radius: 2px;
 }
 
-#softwares > img:nth-child(3){
-  filter: invert(40%) sepia(79%) saturate(3883%) hue-rotate(179deg) brightness(125%) contrast(94%);
-  padding: 3px;
-}
-#softwares > img:nth-child(7){
-  padding: 3px;
-}
-#gmaes-head{
+#gmaes-head {
   padding-top: 10px;
 }
-#games{
+
+#games {
   display: flex;
   align-items: center;
   gap: 12px;
@@ -308,7 +342,7 @@ body {
   z-index: 9;
 }
 
-#games > img{
+#games>img {
   height: 36px;
   border-radius: 8px;
 }
@@ -392,6 +426,7 @@ body {
 }
 
 @keyframes tail-fade {
+
   0%,
   50% {
     width: var(--star-tail-length);
@@ -415,14 +450,16 @@ body {
     opacity: 0.6;
   }
 }
-.left > ol {
+
+.left>ol {
   font-size: 20px;
   display: flex;
   flex-direction: column;
   margin-left: 40px;
   gap: 5px;
 }
-.left > ol > li {
+
+.left>ol>li {
   background: $surface;
   width: max-content;
   backdrop-filter: blur(10px);
@@ -431,18 +468,21 @@ body {
   border-radius: 12px;
   position: relative;
 }
-.left > ol > li > i {
+
+.left>ol>li>i {
   width: 30px;
   display: inline-flex;
   justify-content: center;
   color: $primary;
 }
-.left > ol > li > a {
+
+.left>ol>li>a {
   color: $primary;
   text-decoration: none;
   cursor: pointer;
 }
-.left > ol > li > a > i {
+
+.left>ol>li>a>i {
   position: absolute;
   top: 50%;
   transform: translateY(-50%) translateX(-40%);
@@ -450,7 +490,8 @@ body {
   transition: all 250ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
   opacity: 0;
 }
-.left > ol > li > a:hover > i {
+
+.left>ol>li>a:hover>i {
   transform: translateY(-50%) translateX(120%);
   opacity: 1;
 }
@@ -460,6 +501,7 @@ body {
   display: grid;
   justify-content: center;
 }
+
 main {
   gap: 20px;
   display: flex;
@@ -468,20 +510,25 @@ main {
   align-items: center;
   max-width: $max-width;
 }
+
 .left {
   width: 100%;
 }
-.left > h4 {
+
+.left>h4 {
   opacity: 0.65;
 }
+
 .contact {
   padding-top: 30px;
 }
+
 .title {
   font-family: "Space Grotesk", monospace;
   font-weight: 700;
   font-size: 300%;
 }
+
 p {
   font-weight: 400;
   font-size: 40%;
@@ -499,6 +546,7 @@ p {
   font-size: 200%;
   width: 100%;
 }
+
 #cards {
   display: flex;
   flex-wrap: wrap;
@@ -507,7 +555,7 @@ p {
   width: calc(100% - 20px);
 }
 
-#cards:hover > .card::after {
+#cards:hover>.card::after {
   opacity: 1;
 }
 
@@ -542,24 +590,20 @@ p {
 }
 
 .card::before {
-  background: radial-gradient(
-    800px circle at var(--mouse-x) var(--mouse-y),
-    rgba(255, 255, 255, 0.1),
-    transparent 40%
-  );
+  background: radial-gradient(800px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.1),
+      transparent 40%);
   z-index: 3;
 }
 
 .card::after {
-  background: radial-gradient(
-    600px circle at var(--mouse-x) var(--mouse-y),
-    hsl(227, 41%, 49%, 0.4),
-    transparent 40%
-  );
+  background: radial-gradient(600px circle at var(--mouse-x) var(--mouse-y),
+      hsl(227, 41%, 49%, 0.4),
+      transparent 40%);
   z-index: 1;
 }
 
-.card > .card-content {
+.card>.card-content {
   background-color: #11111188;
   opacity: 1;
   border-radius: inherit;
@@ -598,26 +642,27 @@ span {
   gap: 10px;
 }
 
-.card-info > i {
+.card-info>svg {
   font-size: 1em;
   height: 20px;
   line-height: 20px;
+  fill: $primary;
 }
 
-.card-info-title > h3 {
+.card-info-title>h3 {
   font-size: 1.1em;
+  color: $primary;
   line-height: 20px;
+  transition: all cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
 
-.card-info-title > h4 {
+.card-info-title>h4 {
   color: $text;
   opacity: 0.7;
   font-size: 0.85em;
   margin-top: 8px;
 }
 
-/* -- ↓ ↓ ↓ some responsiveness ↓ ↓ ↓ -- */
-
-</style>
+/* -- ↓ ↓ ↓ some responsiveness ↓ ↓ ↓ -- */</style>
 
 <style scoped></style>
